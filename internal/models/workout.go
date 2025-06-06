122ae55962f4ffe9bbdfaa5b6ce2d44272c43b9c
Michael Cockcroft <cocymsc@hotmail.com>
2025-06-06 17:26:45 +0100

    updated workout type

--- internal/models/workout.go
+++ internal/models/workout.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Workout struct {
-	UserID    string     `json:"userId" validate:"required"`
-	WorkoutID string     `json:"workoutId" validate:"required"`
+	UserID    string     `json:"userId" dynamodbav:"UserID" validate:"required"`
+	WorkoutID string     `json:"workoutId" dynamodbav:"WorkoutID" validate:"required"`
 	Name      string     `json:"name" validate:"required, min=1,max=100"`
 	Exercises []Exercise `json:"exercises"`
 	CreatedAt time.Time  `json:"createdAt"`
